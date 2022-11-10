d924a73489d8a63d8d29603f2fc2174b8fdc3628
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-11-10 09:34:14 +0100

    08 JS adding Examples

--- 008twoFunctionsOneObject.js
+++ 008twoFunctionsOneObject.js
@@ -3,3 +3,15 @@
 /*
 Es posible por que si la fuction devuelve un obj() new lo devuelve y no el this
 */
+
+let obj = {};
+
+function A() { 
+    return obj; 
+}
+
+function B() { 
+    return obj; 
+}
+
+alert( new A() == new B() ); // ESTO DA TRUE
