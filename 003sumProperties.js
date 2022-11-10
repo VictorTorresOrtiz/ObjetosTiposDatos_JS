a8371654cabe185a12828df71df3ab312ad6d545
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2022-11-10 09:04:04 +0100

    03 JS

--- 003sumProperties.js
+++ 003sumProperties.js
@@ -1,3 +1,16 @@
 'use strict'
 
+let salaries = {
+    Harry: 100,
+  Ron 160,
+  Hermione: 130
+  };
+  
+  let sum = 0;
+  for (let key in salaries) {
+    sum += salaries[key];
+  }
+  
+  alert(sum); 
+  
 
