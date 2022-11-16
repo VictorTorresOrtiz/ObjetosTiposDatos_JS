d49769bbe1544a1eda35f83b7f433154faed8d82
Víctor Torres Ortiz <114647477+VictorTorresOrtiz@users.noreply.github.com>
2022-11-16 17:46:45 +0000

    012

--- 012testCuriosity.js
+++ 012testCuriosity.js
@@ -1,3 +1,8 @@
 "use strict";
 
-alert( 9999999999999999 ); ///MIRAR ESTE EJERCICIO
+alert( 9999999999999999 ); 
+
+//AL SER UN NUMERO TAN GRANDE INTRODUCIMOS EL BIG INT
+
+let enterosGrandes = BigInt("9999999999999999");
+alert(enterosGrandes);
